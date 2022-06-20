88b52b3eac7a97f8f39e22f3b9a4683e7c6b67dd
jspaniac <jspaniac@cs.washington.edu>
2022-06-20 02:56:44 -0700

    Updating for cringe classification

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os, json, random, time, datetime, re
+from bayes import NaiveBayes
 import discord
 from discord.ext import commands
 
@@ -12,6 +13,10 @@ bot = commands.Bot(command_prefix='*', intents=intents)
 message_odds = 0.4      # Percentage odds of sending a message over a file
 time_min = '20:00'      # Minimum time to flame people for good morning message
 embed_max = 4096        # Maximum length of embed descriptions
+
+db_file = 'database.json'   # File name for database
+auth_file = 'auth.json'     # File name for auth
+base_dir = './data'         # Base directory for naive bayes
 
 #--------------------#
 # Loading from files #
@@ -119,8 +124,7 @@ async def export(ctx, limit:int=200):
     try:
         with open('log.txt', 'w') as log:
             messages = await ctx.channel.history(limit=limit).flatten()
-            results = {re.sub('<[^>]+>', '', msg.content).replace("\n", " ")
-                         .strip() for msg in messages if not msg.author.bot and
+            results = {format_line(msg.content) for msg in messages if not msg.author.bot and
                                                          not msg.content.startswith('!') and
                                                          not msg.content.startswith('*')}
             log.write("\n".join(results))
@@ -131,6 +135,10 @@ async def export(ctx, limit:int=200):
             os.remove('log.txt')
     except Exception as e:
         print(e)
+
+# Removes all text between <> characters, fits everything to one line, and removes leading/trailing spaces
+def format_line(line):
+    return re.sub('<[^>]+>', '', line).replace("\n", " ").strip()
 
 """
 Currently commented out to see what accumulates naturally
@@ -152,14 +160,30 @@ async def score(ctx):
 # SOCIAL CREDIT RELATED #
 #-----------------------#
 
-@bot.event
-async def on_ready():
-    print("Bot finished loading external files!")
+nb = NaiveBayes()
+nb.train(os.path.join(base_dir, "train", "cringe.txt"),
+         os.path.join(base_dir, "train", "based.txt"))
+
+def test_cringe(file):
+    total = 0
+    cringe = 0
+
+    with open(file) as f:
+        for line in f:
+            formatted = format_line(line)
+            total += 1
+            if nb.predict(formatted):
+                cringe += 1
+                #print(formatted)
+    return cringe / total
+
+print("Cringe accuracy: " + str(test_cringe(os.path.join(base_dir, "test", "cringe.txt"))))
+print("Based accuracy: " + str(1 - test_cringe(os.path.join(base_dir, "test", "based.txt"))))
 
 # Saves database with updated scores
 @bot.event
 async def close():
-    with open('database.json', "w") as db:
+    with open(db_file, "w") as db:
         json.dump(user_to_credit, db)
 
 # General responses to messages sent as non-commands
@@ -171,14 +195,26 @@ async def on_message(message):
     if "good morning" in lower and time.strftime('%H:%M') > time_min:
         await message.channel.send("It's " + time.strftime('%H:%M') + "...")
     
-    # Secretly calculates based on penalties file (temp while working on RNN for)
+    # Secretly tallies if a message is considered cringe or not
+    formatted = format_line(message.content)
+    if nb.predict(formatted):
+        user_to_credit[str(message.author.id)] = user_to_credit.get(str(message.author.id), 0) + 1
+    
+    """
+    # Secretly calculates based on penalties file
     for word, penalty in word_to_penalty.items():
         if word in lower:
             user_to_credit[str(message.author.id)] = user_to_credit.get(str(message.author.id), 0) + penalty
-    
+    """
+
     # Continue processing other commands
     await bot.process_commands(message)
 
-auth = json.load(open('auth.json'))
+# Prints out when the bot is up and running!
+@bot.event
+async def on_ready():
+    print("Bot finished loading external files!")
+
+auth = json.load(open(auth_file))
 TOKEN = auth['token']
 bot.run(TOKEN)
